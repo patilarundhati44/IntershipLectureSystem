09b9b5a9b793dbae3e6d35071aa88531ff4b54b8
patilarundati44 <patilarundhati44@gmail.com>
2025-07-15 16:51:48 +0530

    JPAUtil.java

--- src/main/java/com/example/internship/util/JPAUtil.java
+++ src/main/java/com/example/internship/util/JPAUtil.java
@@ -6,11 +6,20 @@ import jakarta.persistence.Persistence;
 public class JPAUtil 
 {
 	
-	 private static final EntityManagerFactory emf = 
-		        Persistence.createEntityManagerFactory("InternshipPU");
+	 private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("InternshipPU");
 
-		    public static EntityManagerFactory getEntityManagerFactory() {
+		    public static EntityManagerFactory getEntityManagerFactory()
+	
+	            {
 		        return emf;
 		    }
+
+	 public static void shutdown() 
+    {
+        if (emf != null) 
+        {
+            emf.close();
+        }
+    }
 
 }
